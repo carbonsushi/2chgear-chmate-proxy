44b449bda6dc7c64321dfc848ecb90a258bba2b5
carbonsushi <94692480+carbonsushi@users.noreply.github.com>
2022-06-15 12:24:39 +0900

    v1.3

--- app/src/main/java/io/github/carbonsushi/x2chgearchmateproxy/LicenseActivity.kt
+++ app/src/main/java/io/github/carbonsushi/x2chgearchmateproxy/LicenseActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
-class MainActivity : AppCompatActivity() {
+class LicenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
--- app/src/main/java/io/github/carbonsushi/x2chgearchmateproxy/ProxyActivity.kt
+++ app/src/main/java/io/github/carbonsushi/x2chgearchmateproxy/ProxyActivity.kt
@@ -3,22 +3,36 @@ package io.github.carbonsushi.x2chgearchmateproxy
 import android.content.ActivityNotFoundException
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class ProxyActivity : AppCompatActivity() {
+    private val activityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            setResult(result.resultCode)
+            finish()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
-            startActivity(
-                intent.setClassName(
-                    "jp.emprise.android.x2chGear",
-                    "info.narazaki.android.tuboroid.core.activity.common.MessageInfoPostActivity"
-                )
+        val sendIntent = if (intent.data!!.pathSegments.contains("read.cgi")) {
+            intent.setClassName(
+                "jp.emprise.android.x2chGear",
+                "info.narazaki.android.tuboroid.core.activity.common.MessageInfoPostActivity"
             )
+        } else {
+            intent.setClassName(
+                "jp.co.airfront.android.a2chMate",
+                "jp.syoboi.a2chMate.activity.ResEditActivity"
+            )
+        }
+
+        try {
+            activityResult.launch(sendIntent)
         } catch (e: ActivityNotFoundException) {
             Toast.makeText(this, R.string.activity_not_found_error, Toast.LENGTH_LONG).show()
+            finish()
         }
-        finish()
     }
 }
